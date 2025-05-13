c7e83279c8558c93428cff9342e7140d3154b39e
shaito <106555513+0xShaito@users.noreply.github.com>
2025-05-13 10:36:11 -0300

    Revert "chore: merge dev to main (#6)"
    This reverts commit 5ce917aa60272106a33c1d1da1dda24f48ca1996.

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { BenchmarkBase as Benchmark, BenchmarkContext } from './types.js';
-export type { ProfileReport, ProfileResult, GateCount } from './types.js';
-export { Profiler } from './profiler.js';
--- dist/types.js
+++
@@ -1,3 +0,0 @@
-/** Abstract class for users to extend */
-export class BenchmarkBase {
-}
